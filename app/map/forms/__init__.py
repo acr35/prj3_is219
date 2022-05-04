5c1d79bdac2a7dba240a171cce858559e478adee
acr35 <acr35@njit.edu>
2022-05-04 18:37:48 -0400

    added locations form to map

--- app/map/forms/__init__.py
+++ app/map/forms/__init__.py
@@ -5,3 +5,24 @@ from wtforms.fields import *
 class csv_upload(FlaskForm):
   file = FileField()
   submit = SubmitField()
+
+class location_edit_form(FlaskForm):
+    about = TextAreaField('About', [validators.length(min=6, max=300)],
+                          description="Please add information about yourself")
+    is_admin = BooleanField('Admin', render_kw={'value': '1'})
+    submit = SubmitField()
+
+
+class register_form(FlaskForm):
+  email = EmailField('Email Address', [
+    validators.DataRequired(),
+
+  ], description="You need to signup with an email")
+
+  password = PasswordField('Create Password', [
+    validators.DataRequired(),
+    validators.EqualTo('confirm', message='Passwords must match'),
+
+  ], description="Create a password ")
+  confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
+  submit = SubmitField()
